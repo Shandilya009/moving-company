ce3b67da399db3c68d3c8d248562ad029b38e9af
vanisha <143019065+Van1sha@users.noreply.github.com>
2025-04-18 15:08:51 +0530

    Add files via upload

---
+++ db_connect.php
@@ -0,0 +1,17 @@
+<?php
+// Database connection parameters
+$host = 'localhost';
+$dbname = 'easymovers';
+$username = 'root';
+$password = '';
+
+// Create connection
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch(PDOException $e) {
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+?>
